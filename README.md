1befd1e9ab8756c0303f7678e37c1f34f00227f8
Julien Reichardt <mi@jrei.ch>
2017-06-28 23:21:49 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# urandgen.sh
+Generate random numbers in base32 or base64 with /dev/urandom
+# Usage
+```
+urandgen.sh [string length] [base]
+string length: integer number
+base: 32 or 64
+```
--- urandgen.sh
+++ urandgen.sh
@@ -3,6 +3,7 @@
 case $1 in
 	-h|--help|'') echo "$0 - generate random numbers in base32 or base64 with /dev/urandom
 Usage: $0 [string length] [base]
+string length: integer number
 base: 32 or 64"; exit 0;;
 	*[!0-9]*) echo "error: $1 is not an interger"; exit 1;;
 esac
